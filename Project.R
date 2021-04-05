3538072b42ba762f88a1876acbf795fa6ba887bc
Ethan Hackmeyer <ejh16320@uga.edu>
2021-04-05 14:57:58 -0400

    Ethan progress on OA analysis

---
+++ Project.R
@@ -0,0 +1,36 @@
+# Script by Ethan Hackmeyer with a lot of help from Lewis Bartlett
+# ejh16320@uga.edu , lewis.bartlett@uga.edu
+# Oxalic Acid Shop Towel study
+
+#Reading in the 2020 Shearer Trial Data
+
+UGAdata.2020 <- read.csv(file = 'ShearerTrialData.csv', header = T, stringsAsFactors = F)
+
+#Creating a column for the change in percent mite increase
+
+UGAdata.2020$DeltaPMI <- NA
+
+#Populating DeltaPMI by taking the PMI at the end minus the PMI at the beginning of the experiment
+
+for(E in 1:NROW(UGAdata.2020)){
+  UGAdata.2020$DeltaPMI[E] <- (UGAdata.2020$MPB2[which(UGAdata.2020$Colony == UGAdata.2020$Colony[E])]
+                               -
+                                UGAdata.2020$MPB1[which(UGAdata.2020$Colony == UGAdata.2020$Colony[E])]
+                               )
+}
+
+shapiro.test(UGAdata.2020$DeltaPMI)
+hist(UGAdata.2020$DeltaPMI)
+
+#Constructing a linear model and creating an ANOVA
+
+Model1 <- glm(
+  DeltaPMI ~ Yard + TreatmentCode,
+  family = 'gaussian',
+  data = UGAdata.2020
+)
+anova(Model1, test = 'F')
+
+boxplot(UGAdata.2020$DeltaPMI ~ UGAdata.2020$TreatmentCode)
+boxplot(UGAdata.2020$DeltaPMI ~ UGAdata.2020$OxalicDose)
+boxplot(UGAdata.2020$DeltaPMI ~ UGAdata.2020$GlycerinTowel)
